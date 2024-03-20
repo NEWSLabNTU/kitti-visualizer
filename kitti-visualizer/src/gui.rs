164e95a21fe9b2f2570f6a0edf584f328a305d15
aeon <jerry73204@gmail.com>
2024-03-20 09:09:51 +0800

    Speed up rendering

--- kitti-visualizer/src/gui.rs
+++ kitti-visualizer/src/gui.rs
@@ -16,6 +16,7 @@ use kiss3d_utils::WindowPlotExt;
 use kitti_format::KittiObject;
 use nalgebra as na;
 use once_cell::sync::Lazy;
+use rayon::prelude::*;
 use scarlet::{
     color::RGBColor,
     colormap::{ColorMap, ListedColorMap},
@@ -24,6 +25,7 @@ use std::{
     collections::HashMap,
     path::PathBuf,
     rc::Rc,
+    sync::Once,
     time::{Duration, Instant},
 };
 use uluru::LRUCache;
@@ -57,7 +59,7 @@ struct GuiOptions {
 
 struct GuiCache {
     frame_idx: FrameIndex,
-    frame_cache: HashMap<FrameIndex, FrameData>,
+    frame_cache: HashMap<FrameIndex, FramePlot>,
     lru: LRUCache<FrameIndex, 32>,
     next_tick: Option<Instant>,
 }
@@ -69,6 +71,24 @@ struct GuiData {
     supervisely_ann_dir: Option<PathBuf>,
     screencast_dir: Option<PathBuf>,
     pcd_format: PcdFormat,
+}
+
+struct FramePlot {
+    points: Vec<PointPlot>,
+    bboxes: Vec<BoxPlot>,
+}
+
+struct PointPlot {
+    pos: na::Point3<f32>,
+    color: na::Point3<f32>,
+}
+
+struct BoxPlot {
+    box_edges: Vec<[na::Point3<f32>; 2]>,
+    box_color: na::Point3<f32>,
+    text: String,
+    text_color: na::Point3<f32>,
+    text_pos: na::Point3<f32>,
 }
 
 impl Gui {
@@ -83,27 +103,39 @@ impl Gui {
         let ann_dir = kitti_dir.join("label_2");
         let indices = get_indices_from_ann_dir(&ann_dir);
 
-        let Some(&ann_idx) = indices.first() else {
+        if indices.is_empty() {
             bail!(
                 "Unable to load annotation data from {}. Is it empty?",
                 ann_dir.display()
             );
-        };
+        }
 
-        let frame_data = get_new_frame_data(
-            ann_idx as i32,
-            &kitti_dir,
-            supervisely_ann_dir.as_deref(),
-            pcd_format,
-        );
-
-        let frame_data = match frame_data {
-            Ok(frame) => Some(frame),
-            Err(err) => {
-                eprintln!("fail to load frame {ann_idx}: {err}");
-                None
+        let record = match (record_on_start, screencast_dir.is_some()) {
+            (true, true) => true,
+            (true, false) => {
+                static WARN: Once = Once::new();
+                WARN.call_once(|| {
+                    eprintln!("Warning: Request --record-on-start but --screencast-dir is not set");
+                });
+                false
             }
+            (false, _) => false,
         };
+
+        // let frame_data = get_new_frame_data(
+        //     ann_idx as i32,
+        //     &kitti_dir,
+        //     supervisely_ann_dir.as_deref(),
+        //     pcd_format,
+        // );
+
+        // let frame_data = match frame_data {
+        //     Ok(frame) => {},
+        //     Err(err) => {
+        //         eprintln!("fail to load frame {ann_idx}: {err}");
+        //         None
+        //     }
+        // };
 
         let camera = {
             let eye = na::Point3::from_slice(&[-20.0f32, -20.0, 20.0]);
@@ -115,25 +147,19 @@ impl Gui {
 
         let frame_idx = 0;
 
-        let mut lru = LRUCache::default();
-        if frame_data.is_some() {
-            lru.insert(frame_idx);
-        }
+        let lru = LRUCache::default();
 
         Ok(Self {
             cache: GuiCache {
                 frame_idx,
-                frame_cache: match frame_data {
-                    Some(frame_data) => [(frame_idx, frame_data)].into_iter().collect(),
-                    None => HashMap::new(),
-                },
+                frame_cache: HashMap::new(),
                 lru,
                 next_tick: None,
             },
             options: GuiOptions {
                 draw_in_intensity: false,
                 play: play_on_start,
-                record: record_on_start,
+                record,
             },
             data: GuiData {
                 indices,
@@ -171,125 +197,57 @@ impl Gui {
             window.draw_line(
                 &RANGE_VERTEX[i],
                 &RANGE_VERTEX[(i + 1) % 4],
-                &na::Point3::from([0., 0., 0.]),
+                &[0., 0., 0.].into(),
             );
         }
 
         window.draw_axes(na::Point3::origin(), 5.0);
-        self.draw_frame(window);
+        self.render_frame(window);
     }
 
-    fn draw_frame(&self, window: &mut Window) {
+    fn render_frame(&self, window: &mut Window) {
         let Self {
-            options: GuiOptions {
-                draw_in_intensity, ..
-            },
             cache:
                 GuiCache {
                     frame_idx,
                     ref frame_cache,
                     ..
                 },
-            data: GuiData { ref color_map, .. },
             ..
         } = *self;
 
-        let Some(frame_data) = frame_cache.get(&frame_idx) else {
+        let Some(frame) = frame_cache.get(&frame_idx) else {
             return;
         };
-
-        frame_data.points_in_range.iter().for_each(|point| {
-            let color = if draw_in_intensity {
-                let color: RGBColor =
-                    color_map.transform_single((point.intensity / 255. * 10.) as f64);
-                na::Point3::from([color.r, color.g, color.b]).cast()
-            } else {
-                na::Point3::from([0.0, 0.0, 1.0])
-            };
-            window.draw_point(&point.point, &color)
-        });
-        frame_data.points_out_range.iter().for_each(|point| {
-            let color = if draw_in_intensity {
-                let color: RGBColor =
-                    color_map.transform_single((point.intensity / 255. * 10.) as f64);
-                na::Point3::from([color.r, color.g, color.b]).cast()
-            } else {
-                na::Point3::from([0.0, 0.0, 1.0])
-            };
-            window.draw_point(&point.point, &color)
-        });
-        self.draw_objects_in_pcd(&frame_data.objects, &frame_data.num_points_map, window);
+        self.draw_frame(frame, window);
     }
 
-    pub fn draw_objects_in_pcd(
-        &self,
-        objects: &[KittiObject],
-        _num_points_map: &[usize],
-        window: &mut Window,
-    ) {
-        let edge_relation = vec![
-            (0, 1),
-            (0, 2),
-            (1, 3),
-            (2, 3),
-            (4, 5),
-            (4, 6),
-            (5, 7),
-            (6, 7),
-            (0, 4),
-            (1, 5),
-            (2, 6),
-            (3, 7),
-            (1, 7),
-            (3, 5),
-        ];
-        // let rect2velo = {
-        //     let rect_to_cam = na::UnitQuaternion::from_matrix(&calib.r0_rect).inverse();
-        //     let velo_to_cam_rot = na::UnitQuaternion::from_matrix(&na::Matrix3::from_row_slice(&[
-        //         calib.velo_to_cam.m11,
-        //         calib.velo_to_cam.m12,
-        //         calib.velo_to_cam.m13,
-        //         calib.velo_to_cam.m21,
-        //         calib.velo_to_cam.m22,
-        //         calib.velo_to_cam.m23,
-        //         calib.velo_to_cam.m31,
-        //         calib.velo_to_cam.m32,
-        //         calib.velo_to_cam.m33,
-        //     ]));
-        //     let velo_to_cam_trans = na::Translation3::from([
-        //         calib.velo_to_cam.m14,
-        //         calib.velo_to_cam.m24,
-        //         calib.velo_to_cam.m34,
-        //     ]);
-        //     let velo_to_cam = na::Isometry3::from_parts(velo_to_cam_trans, velo_to_cam_rot);
-        //     velo_to_cam.inverse() * rect_to_cam
-        // };
-        for obj in objects {
-            let vertices = obj.bbox3d.vertices();
-            edge_relation.iter().for_each(|(from_idx, to_idx)| {
-                let color = na::Point3::from([0., 1., 0.]);
-                window.draw_line(
-                    &vertices[*from_idx].cast::<f32>(),
-                    &vertices[*to_idx].cast::<f32>(),
-                    &color,
-                );
-            });
-            // let num_points = num_points_map[idx];
-            let text = format!("{:?}, {:.2}", obj.class.clone(), obj.bbox3d.extents.x);
-            let color = if obj.object_key.is_some() {
-                na::Point3::from([1., 0., 0.])
-            } else {
-                na::Point3::from([0., 0., 0.])
-            };
-            self.draw_text_3d(
-                window,
-                &text,
-                &na::Point3::cast(obj.bbox3d.pose.translation.vector.into()),
-                50.0,
-                &Font::default(),
-                &color,
-            );
+    fn draw_frame(&self, frame: &FramePlot, window: &mut Window) {
+        let FramePlot { points, bboxes } = frame;
+
+        for PointPlot { pos, color } in points {
+            window.draw_point(pos, color)
         }
+
+        for bbox in bboxes {
+            self.draw_bbox(bbox, window);
+        }
+    }
+
+    fn draw_bbox(&self, bbox: &BoxPlot, window: &mut Window) {
+        let BoxPlot {
+            box_edges,
+            text,
+            text_color,
+            box_color,
+            text_pos,
+        } = bbox;
+
+        for [p, q] in box_edges {
+            window.draw_line(p, q, box_color);
+        }
+
+        self.draw_text_3d(window, text, text_pos, 50.0, &Font::default(), text_color);
     }
 
     fn draw_text_3d(
@@ -336,7 +294,11 @@ impl Gui {
             data,
             ..
         } = self;
-        let GuiData { ref indices, .. } = *data;
+        let GuiData {
+            ref indices,
+            ref screencast_dir,
+            ..
+        } = *data;
 
         let orig_frame_idx = *frame_idx;
         let mut new_frame_idx = *frame_idx;
@@ -348,6 +310,13 @@ impl Gui {
 
             match event.value {
                 E::Key(K::R, A::Press, _) => {
+                    if let (false, None) = (*record, screencast_dir) {
+                        static WARN: Once = Once::new();
+                        WARN.call_once(|| {
+                            eprintln!("Warning: Request to record screencast but --screencast-dir is not set");
+                        });
+                    }
+
                     *record = !*record;
                 }
                 E::Key(K::I, A::Press, _) => {
@@ -397,10 +366,14 @@ impl Gui {
         }
 
         *frame_idx = new_frame_idx;
-        self.load_frame_data(new_frame_idx);
     }
 
-    fn load_frame_data(&mut self, frame_idx: usize) {
+    fn update(&mut self) {
+        let Self {
+            cache: GuiCache { frame_idx, .. },
+            ..
+        } = *self;
+
         use std::collections::hash_map::Entry;
         let Self {
             cache:
@@ -414,9 +387,13 @@ impl Gui {
                     ref indices,
                     ref kitti_dir,
                     ref supervisely_ann_dir,
+                    ref color_map,
                     pcd_format,
                     ..
                 },
+            options: GuiOptions {
+                draw_in_intensity, ..
+            },
             ..
         } = *self;
         let ann_idx = indices[frame_idx];
@@ -429,18 +406,21 @@ impl Gui {
                 pcd_format,
             );
 
-            match result {
-                Ok(frame_data) => {
-                    entry.insert(frame_data);
-
-                    if let Some(frame_idx_to_rm) = lru.insert(frame_idx) {
-                        frame_cache.remove(&frame_idx_to_rm);
-                    }
-                }
+            let frame_data = match result {
+                Ok(frame_data) => frame_data,
                 Err(err) => {
                     eprintln!("fail to load frame {}: {err}", ann_idx);
+                    return;
                 }
             };
+
+            let frame_plot = convert_frame(&frame_data, draw_in_intensity, color_map);
+
+            entry.insert(frame_plot);
+
+            if let Some(frame_idx_to_rm) = lru.insert(frame_idx) {
+                self.cache.frame_cache.remove(&frame_idx_to_rm);
+            }
         }
     }
 }
@@ -448,14 +428,18 @@ impl Gui {
 impl State for Gui {
     fn step(&mut self, window: &mut Window) {
         self.process_events(window);
+        self.update();
         self.render(window);
 
-        if let (true, Some(screencast_dir)) = (self.options.record, &self.data.screencast_dir) {
-            let screenshot = window.snap_image();
-            let image_path = screencast_dir.join(format!("{:06}.jpg", self.cache.frame_idx));
-            if let Err(err) = screenshot.save(&image_path) {
-                eprintln!("unable to save {}: {err}", image_path.display());
+        match (self.options.record, &self.data.screencast_dir) {
+            (true, Some(screencast_dir)) => {
+                let screenshot = window.snap_image();
+                let image_path = screencast_dir.join(format!("{:06}.jpg", self.cache.frame_idx));
+                if let Err(err) = screenshot.save(&image_path) {
+                    eprintln!("unable to save {}: {err}", image_path.display());
+                }
             }
+            _ => {}
         }
     }
 
@@ -469,6 +453,119 @@ impl State for Gui {
     ) {
         (Some(&mut self.camera), None, None, None)
     }
+}
+
+fn convert_frame(
+    frame_data: &FrameData,
+    draw_in_intensity: bool,
+    color_map: &ListedColorMap,
+) -> FramePlot {
+    // let Some(frame_data) = frame_cache.get(&frame_idx) else {
+    //     return;
+    // };
+
+    let points_in_range = frame_data.points_in_range.par_iter().map(|point| {
+        let color = if draw_in_intensity {
+            let color: RGBColor = color_map.transform_single((point.intensity / 255. * 10.) as f64);
+            na::Point3::from([color.r, color.g, color.b]).cast()
+        } else {
+            na::Point3::from([0.0, 0.0, 1.0])
+        };
+        (&point.point, color)
+    });
+
+    let points_out_range = frame_data.points_out_range.par_iter().map(|point| {
+        let color = if draw_in_intensity {
+            let color: RGBColor = color_map.transform_single((point.intensity / 255. * 10.) as f64);
+            na::Point3::from([color.r, color.g, color.b]).cast()
+        } else {
+            na::Point3::from([0.0, 0.0, 1.0])
+        };
+        (&point.point, color)
+    });
+
+    let points: Vec<_> = points_in_range
+        .chain(points_out_range)
+        .map(|(&pos, color)| PointPlot { pos, color })
+        .collect();
+
+    let bboxes = convert_objects_in_pcd(&frame_data.objects, &frame_data.num_points_map);
+
+    FramePlot { points, bboxes }
+}
+
+fn convert_objects_in_pcd(objects: &[KittiObject], _num_points_map: &[usize]) -> Vec<BoxPlot> {
+    const EDGE_RELATION: &[(usize, usize)] = &[
+        (0, 1),
+        (0, 2),
+        (1, 3),
+        (2, 3),
+        (4, 5),
+        (4, 6),
+        (5, 7),
+        (6, 7),
+        (0, 4),
+        (1, 5),
+        (2, 6),
+        (3, 7),
+        (1, 7),
+        (3, 5),
+    ];
+    // let rect2velo = {
+    //     let rect_to_cam = na::UnitQuaternion::from_matrix(&calib.r0_rect).inverse();
+    //     let velo_to_cam_rot = na::UnitQuaternion::from_matrix(&na::Matrix3::from_row_slice(&[
+    //         calib.velo_to_cam.m11,
+    //         calib.velo_to_cam.m12,
+    //         calib.velo_to_cam.m13,
+    //         calib.velo_to_cam.m21,
+    //         calib.velo_to_cam.m22,
+    //         calib.velo_to_cam.m23,
+    //         calib.velo_to_cam.m31,
+    //         calib.velo_to_cam.m32,
+    //         calib.velo_to_cam.m33,
+    //     ]));
+    //     let velo_to_cam_trans = na::Translation3::from([
+    //         calib.velo_to_cam.m14,
+    //         calib.velo_to_cam.m24,
+    //         calib.velo_to_cam.m34,
+    //     ]);
+    //     let velo_to_cam = na::Isometry3::from_parts(velo_to_cam_trans, velo_to_cam_rot);
+    //     velo_to_cam.inverse() * rect_to_cam
+    // };
+
+    let box_plots: Vec<_> = objects
+        .par_iter()
+        .map(|obj| {
+            let vertices = obj.bbox3d.vertices();
+
+            let box_color: na::Point3<f32> = [0., 1., 0.].into();
+            let box_edges: Vec<_> = EDGE_RELATION
+                .par_iter()
+                .copied()
+                .map(|(from_idx, to_idx)| [vertices[from_idx].cast(), vertices[to_idx].cast()])
+                .collect();
+
+            // let num_points = num_points_map[idx];
+            let text = format!("{:?}, {:.2}", obj.class.clone(), obj.bbox3d.extents.x);
+            let text_color: na::Point3<f32> = if obj.object_key.is_some() {
+                [1., 0., 0.]
+            } else {
+                [0., 0., 0.]
+            }
+            .into();
+            let text_pos = na::Point3::from(obj.bbox3d.pose.translation.vector).cast();
+
+            BoxPlot {
+                box_edges,
+                text,
+                text_color,
+                box_color,
+                text_pos,
+            }
+        })
+        .collect();
+
+    box_plots
 }
 
 // pub fn draw_2d_bbox(obj: &KittiObject, image: &mut cv_core::Mat) {
